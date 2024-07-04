e4627791cafc0c28ee0de33fddc6cb3cc94163c6
Sasith Methmal <52422394+sasithmj@users.noreply.github.com>
2024-07-04 18:53:37 +0530

    update and delete

--- routes/ScheduleRoute.js
+++ routes/ScheduleRoute.js
@@ -3,5 +3,7 @@ const scheduleController = require("../controllers/ScheduleController");
 
 route.post("/scheduleWaste", scheduleController.scheduleWaste);
 route.post("/getscheduleWaste", scheduleController.getSchedules);
+route.post("/updatescheduleWaste", scheduleController.updateScheduleDate);
+route.post("/deletescheduleWaste", scheduleController.deleteScheduleData);
 
 module.exports = route;
