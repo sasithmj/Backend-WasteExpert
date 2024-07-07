513e14dac191128dffe0d02f01cfcdbee1edd466
Sasith Methmal <52422394+sasithmj@users.noreply.github.com>
2024-07-08 02:01:07 +0530

    sda

--- app.js
+++ app.js
@@ -10,16 +10,20 @@ const SmartBinRoute = require("./routes/SmartBinRoute");
 const AdminRoute = require("./routes/AdminRoute");
 const DispatcherRoute = require("./routes/DispatcherRoute");
 const CollectorRoute = require("./routes/CollectorRoute");
+const WasteReportRoute = require("./routes/WasteReportRoute");
 
 const cors = require("cors");
 
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "http://localhost:3002",
   optionsSuccessStatus: 200,
   credentials: true,
 };
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
@@ -27,6 +31,7 @@ app.use(express.json());
 app.use("/", userRoute);
 app.use("/admin", AdminRoute);
 app.use("/schedule", scheduleRoute);
+app.use("/reportWaste", WasteReportRoute);
 
 //web
 app.use("/schedulePickup", schedulePickupRoute);
@@ -34,6 +39,5 @@ app.use("/smartbin", SmartBinRoute);
 app.use("/addAdmin", AdminRoute);
 app.use("/addDispatcher", DispatcherRoute);
 app.use("/addCollector", CollectorRoute);
-
 
 module.exports = app;
---
+++ services/WasteReportService.js
@@ -0,0 +1,48 @@
+const ReportSchedule = require("../models/WasteReportModel");
+const User = require("../models/Usermodel");
+
+class WasteReportService {
+  static async reportWaste(
+    UserId,
+    Photo,
+    locationLat,
+    locationLng,
+    ReportDate,
+    Description,
+    WasteTypes
+  ) {
+    try {
+      // Log the data before saving
+      console.log("Data to be saved in ReportService:", {
+        UserId,
+        Photo,
+        locationLat,
+        locationLng,
+        ReportDate,
+        Description,
+        WasteTypes,
+      });
+
+      const newReport = new ReportSchedule({
+        UserId,
+        Photo,
+        locationLat,
+        locationLng,
+        ReportDate,
+        Description,
+        WasteTypes,
+      });
+
+      // Log the new schedule object before saving
+      console.log("New Report Object:", newReport);
+
+      await newReport.save();
+      return { success: true, message: "New Report Placed" };
+    } catch (error) {
+      console.error("Error while saving waste Report:", error);
+      throw new Error(`Error while Report waste: ${error}`);
+    }
+  }
+}
+
+module.exports = WasteReportService;
