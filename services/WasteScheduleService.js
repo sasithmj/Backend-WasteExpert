e57f53dda97977485d98ac8acf2a1c44e9cd7895
Sasith Methmal <52422394+sasithmj@users.noreply.github.com>
2024-07-13 10:21:25 +0530

    location added to schedule

--- services/WasteScheduleService.js
+++ services/WasteScheduleService.js
@@ -12,11 +12,16 @@ class WasteScheduleService {
         ScheduleState,
       });
 
+      const user = await User.findOne({ _id: UserId }).select("-password");
+
+      const location = user.location;
+
       const newSchedule = new WasteSchedule({
         UserId,
         WasteType,
         ScheduledDate,
         ScheduleState,
+        location,
       });
 
       // Log the new schedule object before saving
@@ -73,7 +78,9 @@ class WasteScheduleService {
   static async deleteScheduleData(scheduleId) {
     try {
       // Find the user by email
-      const deleteSchedule = await WasteSchedule.findOneAndDelete({ _id: scheduleId });
+      const deleteSchedule = await WasteSchedule.findOneAndDelete({
+        _id: scheduleId,
+      });
       if (!deleteSchedule) {
         return { success: false, message: "schedule not found" };
       }
